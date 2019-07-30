90db3f6b50b538ec91e6ae5225910a102ff5a8cd
YiShan Lin <a82012844013@gmail.com>
2019-07-30 11:57:36 -0500

    leetcode31

---
+++ leetcode31/leetcode31_my_version.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int nums_size = nums.size();
+        
+        if(nums_size <= 1){
+            return;
+        }
+        
+        //in descending order
+        bool is_descending = true;
+        
+        for(int i = nums_size - 1; i >= 1; i--){
+            if(nums[i] > nums[i-1]){
+                is_descending = false;
+                
+                //get next permutation from nums[i-1] to the end
+                int num_start_for_next_permutation = INT_MAX, next_pos = i;
+                for(int j = i; j < nums_size; j++){
+                    if(nums[j] <= num_start_for_next_permutation && nums[j] > nums[i-1]){
+                        num_start_for_next_permutation = nums[j];
+                        next_pos = j;
+                    }
+                }
+                
+                swap(nums[i-1], nums[next_pos]);
+                sort(nums.begin() + i, nums.end());
+                
+                return;
+            }
+        }
+        
+        //is descending order, than change to ascending order
+        if(is_descending){
+            sort(nums.begin(), nums.end());
+        }
+        
+        return;
+    }
+};
+// Runtime: 8 ms, faster than 79.02% of C++ online submissions for Next Permutation.
+// Memory Usage: 8.7 MB, less than 40.65% of C++ online submissions for Next Permutation.
+// time complexity: O(n + n log n)
+// space complexity: O(1)
