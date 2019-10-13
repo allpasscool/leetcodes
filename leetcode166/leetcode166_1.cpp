799c96f623433625c00b0d91b0069eef46582b46
YiShan Lin <a82012844013@gmail.com>
2019-10-12 23:21:24 -0500

    leetcode166

---
+++ leetcode166/leetcode166_1.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    string fractionToDecimal(int numerator, int denominator) {
+        long long n = numerator, d = denominator;
+        
+        // zero numerator
+        if (n == 0) return "0";
+
+        string res;
+        // determine the sign
+        if (n < 0 ^ d < 0) res += '-';
+
+        // remove sign of operands
+        n = abs(n), d = abs(d);
+
+        // append integral part
+        res += to_string(n / d);
+
+        // in case no fractional part
+        if (n % d == 0) return res;
+
+        res += '.';
+
+        unordered_map<int, int> map;
+
+        // simulate the division process
+        for (int64_t r = n % d; r; r %= d) {
+
+            // meet a known remainder
+            // so we reach the end of the repeating part
+            if (map.count(r) > 0) {
+                res.insert(map[r], 1, '(');
+                res += ')';
+                break;
+            }
+
+            // the remainder is first seen
+            // remember the current position for it
+            map[r] = res.size();
+
+            r *= 10;
+
+            // append the quotient digit
+            res += to_string(r / d);
+        }
+
+        return res;
+    }
+};
+
+// Runtime: 4 ms, faster than 58.63% of C++ online submissions for Fraction to Recurring Decimal.
+// Memory Usage: 8.9 MB, less than 93.33% of C++ online submissions for Fraction to Recurring Decimal.
+// time complexity: O(n), n is time we need to find the first iteration
+// space complexity: O(n)
