907dda34d1d12c224babe8c3bf44d823b8d1fd39
allpasscool <a82012844013@yahoo.com.tw>
2021-04-10 18:13:19 -0700

    leetcode242

---
+++ leetcode242/leetcode242_my_version_2.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+        
+        HashMap<Character, Integer> alpha_count = new HashMap<>();
+        
+        // count s
+        for(int i = 0; i < s.length(); i++) {
+            char alpha = (char)(s.charAt(i) - 'a');
+            
+            if(alpha_count.containsKey(alpha))
+                alpha_count.put(alpha, alpha_count.get(alpha)+1);
+            else
+                alpha_count.put(alpha, 1);
+        }
+        
+        // used on t
+        for(int i = 0; i < t.length(); i++) {
+            char alpha = (char)(t.charAt(i) - 'a');
+            
+            if(alpha_count.containsKey(alpha))
+                alpha_count.put(alpha, alpha_count.get(alpha)-1);
+            else
+                return false;
+            
+            if(alpha_count.get(alpha) < 0)
+                return false;
+        }
+        
+        return true;
+    }
+}
+
+
+// Runtime: 13 ms, faster than 12.72% of Java online submissions for Valid Anagram.
+// Memory Usage: 39.7 MB, less than 18.51% of Java online submissions for Valid Anagram.
+// time complexity: O(n)
+// spcae complexity: O(1)
