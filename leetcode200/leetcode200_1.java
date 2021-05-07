fc960a6cbcf82915b2448f09bee6a52c8f21ddef
allpasscool <a82012844013@yahoo.com.tw>
2021-05-06 23:07:04 -0700

    leetcode200

--- leetcode200/leetcode200_1.java
+++ leetcode200/leetcode200_1.java
@@ -1,39 +1,39 @@
 class Solution {
-    void dfs(char[][] grid, int r, int c) {
-      int nr = grid.length;
-      int nc = grid[0].length;
-  
-      if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
-        return;
-      }
-  
-      grid[r][c] = '0';
-      dfs(grid, r - 1, c);
-      dfs(grid, r + 1, c);
-      dfs(grid, r, c - 1);
-      dfs(grid, r, c + 1);
+  void dfs(char[][] grid, int r, int c) {
+    int nr = grid.length;
+    int nc = grid[0].length;
+
+    if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
+      return;
     }
-  
-    public int numIslands(char[][] grid) {
-      if (grid == null || grid.length == 0) {
-        return 0;
-      }
-  
-      int nr = grid.length;
-      int nc = grid[0].length;
-      int num_islands = 0;
-      for (int r = 0; r < nr; ++r) {
-        for (int c = 0; c < nc; ++c) {
-          if (grid[r][c] == '1') {
-            ++num_islands;
-            dfs(grid, r, c);
-          }
+
+    grid[r][c] = '0';
+    dfs(grid, r - 1, c);
+    dfs(grid, r + 1, c);
+    dfs(grid, r, c - 1);
+    dfs(grid, r, c + 1);
+  }
+
+  public int numIslands(char[][] grid) {
+    if (grid == null || grid.length == 0) {
+      return 0;
+    }
+
+    int nr = grid.length;
+    int nc = grid[0].length;
+    int num_islands = 0;
+    for (int r = 0; r < nr; ++r) {
+      for (int c = 0; c < nc; ++c) {
+        if (grid[r][c] == '1') {
+          ++num_islands;
+          dfs(grid, r, c);
         }
       }
-  
-      return num_islands;
     }
+
+    return num_islands;
   }
+}
 
 // Runtime: 1 ms, faster than 100.00% of Java online submissions for Number of Islands.
 // Memory Usage: 41 MB, less than 98.16% of Java online submissions for Number of Islands.
