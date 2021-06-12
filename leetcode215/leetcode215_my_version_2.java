ef405181b231e31a186ece2744092c272ddf0e5c
allpasscool <a82012844013@yahoo.com.tw>
2021-06-12 01:04:25 -0700

    leetcode215

---
+++ leetcode215/leetcode215_my_version_2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        return nums[n - k];
+    }
+}
+
+// Runtime: 1 ms, faster than 97.97% of Java online submissions for Kth Largest
+// Element in an Array.
+// Memory Usage: 39.4 MB, less than 47.81% of Java online submissions for Kth
+// Largest Element in an Array.
+// tiem complexity: O(n log n)
+// space complexity: O(1)
