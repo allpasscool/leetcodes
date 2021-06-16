0c69203dabfab74df8e07ed5208f0c14525be73e
allpasscool <a82012844013@yahoo.com.tw>
2021-06-16 02:33:49 -0700

    Update leetcode341_2.java

--- leetcode341/leetcode341_2.java
+++ leetcode341/leetcode341_2.java
@@ -22,11 +22,17 @@ public class NestedIterator implements Iterator<Integer> {
 
     // In Java, the Stack class is considered deprecated. Best practice is to use
     // a Deque instead. We'll use addFirst() for push, and removeFirst() for pop.
-    private Deque<NestedInteger> stack;
+    // private Deque<NestedInteger> stack;
+    private Stack<NestedInteger> stack;
 
     public NestedIterator(List<NestedInteger> nestedList) {
         // The constructor puts them on in the order we require. No need to reverse.
-        stack = new ArrayDeque(nestedList);
+        // stack = new ArrayDeque(nestedList);
+        stack = new Stack<NestedInteger>();
+        for (int i = nestedList.size() - 1; i >= 0; i--) {
+            stack.push(nestedList.get(i));
+            // stack.addFirst(nestedList.get(i));
+        }
     }
 
     @Override
@@ -36,7 +42,8 @@ public class NestedIterator implements Iterator<Integer> {
             throw new NoSuchElementException();
         // hasNext ensures the stack top is now an integer. Pop and return
         // this integer.
-        return stack.removeFirst().getInteger();
+        // return stack.removeFirst().getInteger();
+        return stack.pop().getInteger();
     }
 
     @Override
@@ -51,11 +58,14 @@ public class NestedIterator implements Iterator<Integer> {
     private void makeStackTopAnInteger() {
         // While there are items remaining on the stack and the front of
         // stack is a list (i.e. not integer), keep unpacking.
-        while (!stack.isEmpty() && !stack.peekFirst().isInteger()) {
+        // while (!stack.isEmpty() && !stack.peekFirst().isInteger()) {
+        while (!stack.isEmpty() && !stack.peek().isInteger()) {
             // Put the NestedIntegers onto the stack in reverse order.
-            List<NestedInteger> nestedList = stack.removeFirst().getList();
+            List<NestedInteger> nestedList = stack.pop().getList();
+            // List<NestedInteger> nestedList = stack.removeFirst().getList();
             for (int i = nestedList.size() - 1; i >= 0; i--) {
-                stack.addFirst(nestedList.get(i));
+                stack.push(nestedList.get(i));
+                // stack.addFirst(nestedList.get(i));
             }
         }
     }
