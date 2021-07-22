08ca69c4faf2273b8e965cb0a7fecbeef84f49ac
allpasscool <a82012844013@yahoo.com.tw>
2021-07-22 01:29:26 -0700

    leetcode15

---
+++ leetcode15/leetcode15_1.java
@@ -0,0 +1,32 @@
+// Runtime: 15 ms, faster than 99.77% of Java online submissions for 3Sum.
+// Memory Usage: 42.6 MB, less than 92.70% of Java online submissions for 3Sum.
+// time complexity: O(n^2)
+// space complexity: O(1)
+// Solution 1
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length && nums[i] <= 0; ++i)
+            // i == 0 || nums[i] is not equal to previous element
+            if (i == 0 || nums[i - 1] != nums[i]) {
+                twoSumII(nums, i, res);
+            }
+        return res;
+    }
+    void twoSumII(int[] nums, int i, List<List<Integer>> res) {
+        int lo = i + 1, hi = nums.length - 1;
+        while (lo < hi) {
+            int sum = nums[i] + nums[lo] + nums[hi];
+            if (sum < 0) {
+                ++lo;
+            } else if (sum > 0) {
+                --hi;
+            } else {
+                res.add(Arrays.asList(nums[i], nums[lo++], nums[hi--]));
+                while (lo < hi && nums[lo] == nums[lo - 1])
+                    ++lo;
+            }
+        }
+    }
+}
