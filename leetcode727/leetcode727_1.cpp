85cdcf9a6cefeb0e6beb12533d99ff0c957e0d2d
allpasscool <a82012844013@yahoo.com.tw>
2021-09-09 02:02:53 -0700

    leetcode727

--- leetcode727/leetcode727_1.cpp
+++ leetcode727/leetcode727_1.cpp
@@ -1,45 +1,49 @@
 class Solution {
 public:
-    string minWindow(string S, string T) 
-    {
-        int m = S.size();
-        int n = T.size();
-        S = "#" + S;
-        T = "#" + T;
-        auto dp = vector<vector<int>>(m + 1, vector<int>(n + 1, 0));
+    string minWindow(string s1, string s2) {
+        int n1 = s1.size(), n2 = s2.size();
+        s1 = '#' + s1;
+        s2 = '#' + s2;
         
-        for (int j = 1; j <= n; j++)
-            dp[0][j] = INT_MAX / 2;
-        for (int i = 0; i <= m; i++)
-            dp[i][0] = 0;
+        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, INT_MAX / 2));
         
-        for (int i = 1; i <= m; i++)
-            for (int j = 1; j <= n; j++)
-            {
-                if (S[i] == T[j])
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                else
-                    dp[i][j] = dp[i - 1][j] + 1;
-            }
-        
-        int len  = INT_MAX / 2;
-        int pos;
-        for (int i = 1; i <= m; i++)
+        for (int i = 0; i <= n1; i++)
         {
-            if (dp[i][n] < len)
+            dp[i][0] = 0;
+        }
+        
+        
+        for (int i = 1; i <= n1; i++)
+        {
+            for (int j = 1; j <= n2; j++)
             {
-                len = dp[i][n];
-                pos = i;
+                if (s1[i] == s2[j])
+                {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else
+                {
+                    dp[i][j] = dp[i - 1][j] + 1;
+                }
             }
         }
         
-        if (len>=INT_MAX / 2)
-            return "";
-        else
-            return S.substr(pos - len + 1,len);
+        int best = INT_MAX / 2;
+        string ans = "";
         
+        for (int i = 1; i <= n1; i++)
+        {
+            if (dp[i][n2] < best)
+            {
+                best = dp[i][n2];
+                ans = s1.substr(i - dp[i][n2] + 1, dp[i][n2]);
+            }
+        }
+        
+        return ans;
     }
 };
+
 
 // Runtime: 472 ms, faster than 10.92% of C++ online submissions for Minimum Window Subsequence.
 // Memory Usage: 90.3 MB, less than 15.63% of C++ online submissions for Minimum Window Subsequence.
@@ -47,3 +51,47 @@ public:
 // space complexity: O(MN)
 // https://github.com/wisdompeak/LeetCode/tree/master/Greedy/727.Minimum-Window-Subsequence
 // https://www.youtube.com/watch?v=c8AgVpUtkwE&ab_channel=HuifengGuan
+
+
+// class Solution {
+// public:
+//     string minWindow(string S, string T) 
+//     {
+//         int m = S.size();
+//         int n = T.size();
+//         S = "#" + S;
+//         T = "#" + T;
+//         auto dp = vector<vector<int>>(m + 1, vector<int>(n + 1, 0));
+        
+//         for (int j = 1; j <= n; j++)
+//             dp[0][j] = INT_MAX / 2;
+//         for (int i = 0; i <= m; i++)
+//             dp[i][0] = 0;
+        
+//         for (int i = 1; i <= m; i++)
+//             for (int j = 1; j <= n; j++)
+//             {
+//                 if (S[i] == T[j])
+//                     dp[i][j] = dp[i - 1][j - 1] + 1;
+//                 else
+//                     dp[i][j] = dp[i - 1][j] + 1;
+//             }
+        
+//         int len  = INT_MAX / 2;
+//         int pos;
+//         for (int i = 1; i <= m; i++)
+//         {
+//             if (dp[i][n] < len)
+//             {
+//                 len = dp[i][n];
+//                 pos = i;
+//             }
+//         }
+        
+//         if (len>=INT_MAX / 2)
+//             return "";
+//         else
+//             return S.substr(pos - len + 1,len);
+        
+//     }
+// };
--- leetcode727/leetcode727_2.cpp
+++ leetcode727/leetcode727_2.cpp
@@ -55,3 +55,55 @@ public:
 // space complexity: O(M)
 // https://github.com/wisdompeak/LeetCode/tree/master/Greedy/727.Minimum-Window-Subsequence
 // https://www.youtube.com/watch?v=c8AgVpUtkwE&ab_channel=HuifengGuan
+
+// class Solution {
+// public:
+//     string minWindow(string s1, string s2) {
+//         int n1 = s1.size(), n2 = s2.size();
+//         s1 = '#' + s1;
+//         vector<vector<int>> next(n1 + 1, vector<int>(26, -1));
+        
+//         for (int i = n1; i > 0; i--)
+//         {
+//             for (int num = 0; num < 26; num++)
+//             {
+//                 next[i - 1][num] = next[i][num];
+//             }
+//             next[i - 1][s1[i] - 'a'] = i;
+//         }
+        
+//         int best = INT_MAX;
+//         string ans = "";
+//         for (int i = 0; i < n1; i++)
+//         {
+//             if (next[i][s2[0] - 'a'] != -1)
+//             {
+//                 int pos = next[i][s2[0] - 'a'];
+//                 int start = pos;
+//                 int j = 1;
+//                 while (j < n2)
+//                 {
+//                     if (next[pos][s2[j] - 'a'] != -1)
+//                     {
+//                         pos = next[pos][s2[j] - 'a'];
+//                         j++;
+//                     }
+//                     else
+//                     {
+//                         break;
+//                     }
+//                 }
+                
+//                 if (j == n2 && (pos - start + 1) < best)
+//                 {
+//                     best = pos - start + 1;
+//                     ans = s1.substr(start, pos - start + 1);
+//                 }
+                
+//                 i = start - 1;
+//             }
+//         }
+        
+//         return ans;
+//     }
+// };
