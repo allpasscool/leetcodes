879a74f173f4b2273489bd9ef9995b3ddbeb436c
allpasscool <a82012844013@yahoo.com.tw>
2022-01-11 20:14:31 -0800

    leetcode701

---
+++ leetcode701/leetcode701_2.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    TreeNode *insertIntoBST(TreeNode *root, int val)
+    {
+        if (!root)
+            return new TreeNode(val);
+
+        TreeNode *cur = root;
+
+        while (true)
+        {
+            if (val > cur->val)
+            {
+                if (cur->right)
+                    cur = cur->right;
+                else
+                {
+                    cur->right = new TreeNode(val);
+                    break;
+                }
+            }
+            else
+            {
+                if (cur->left)
+                    cur = cur->left;
+                else
+                {
+                    cur->left = new TreeNode(val);
+                    break;
+                }
+            }
+        }
+
+        return root;
+    }
+};
+
+// Runtime: 228 ms, faster than 5.15% of C++ online submissions for Insert into a Binary Search Tree.
+// Memory Usage: 57 MB, less than 49.59% of C++ online submissions for Insert into a Binary Search Tree.
+// time complexity: O(H)
+// space complexity: O(1)
